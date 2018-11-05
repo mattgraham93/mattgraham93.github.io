a9d7dec6c658b82943d5d40f7694a4c3045960cc
matt.graham <matt.graham@bellevuecollege.edu>
2018-11-05 11:25:15 -0800

    fixing js

--- assignments/for-loops/index.js
+++ assignments/for-loops/index.js
@@ -1,9 +1,13 @@
+function addToList() {
+    const myList = document.getElementById('my-list');
+
+    for (let i=0; i < 5; i++) {
+        elfCode.appendToList(myList, i);
+    }
+}
+
 window.onload = () => {
     const listButton = document.getElementById('run-for-loop');
 
-    listButton.onclick = () => {
-        const myList = document.getElementById('my-list');
-
-        for (let i=0; i < 5; i++) elfCode.appendToList(myList, i);
-    }
-}
+    listButton.onclick = addToList;
+};
