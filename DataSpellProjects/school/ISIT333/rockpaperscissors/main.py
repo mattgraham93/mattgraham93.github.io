20a61ff94e4889f1ff1b751a847dc2bf62e6f35a
Matts Blade 15 <matt.graham@bellevuecollege.edu>
2021-11-14 15:03:50 -0800

    adding db and restructure folder for blade 15, added project for directory

---
+++ DataSpellProjects/school/ISIT333/rockpaperscissors/main.py
@@ -0,0 +1,22 @@
+# Name: Matt Graham
+# Date: 10/21/2021
+# Class: ISIT 333
+# Assignment: Project 2 - Rock Paper Scissors
+
+import game
+
+
+# TODO:
+# need title
+# print out directions and describe items, and their pairings
+def main():
+    print("---- Welcome to a World Melting Game :) ----")
+    print("-- This game is similar to a childhood favorite, results are final. :)\n")
+
+    game.game()
+
+    print("Thank you for playing!")
+
+
+if __name__ == "__main__":
+    main()
