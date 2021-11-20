3a894feab09b3bfb9871a98ee2595a9edb1c6dc5
Matts Blade 15 <matt.graham@bellevuecollege.edu>
2021-11-19 20:01:20 -0800

    updated sqlite assignment

--- DataSpellProjects/school/ISIT333/sqlite3/main.py
+++ DataSpellProjects/school/ISIT333/sqlite3/main.py
@@ -2,27 +2,80 @@ import sqlite
 
 
 def main():
+    print("------- START PROGRAM -------\n")
+    print("Welcome to the Pulp Fiction database! It was, at one point, my favorite movie. I don't think it is anymore.")
+    print("Feel free to browse around and learn more about the characters!\n ")
+    # set database name/location
     db_name = "C:/mattgraham93.github.io/DataSpellProjects/data/ISIT333.db"
-    sqlite.connect(db_name)
-
-    user_input = 0
+    table_name = "users"
+    # connect to db and create table
+    print("----------- Establishing connection -----------")
+    conn, cursor = sqlite.connect(db_name)
+    sqlite.create_table(cursor, table_name)
+    print(f"------ Successfully connected to {table_name}! -------\n")
+    # set baseline error count to track when to re-print menu
+    error_count = -1
 
     while True:
-
-        user_input = input("Please select an option:")
-        if user_input == 1:
-            print("option 1")
-        elif user_input == 2:
-            print("option 2")
-        elif user_input == 3:
-            print("option 3")
-        elif user_input == 4:
-            print("option 3")
-        elif user_input == 5:
-            print("Thank you! The connection is now closing.")
-            break
+        # print menu if first load or user has submitted the wrong input 5 times
+        if error_count == -1:
+            menu()
+            error_count += 1
+        elif error_count > 4:
+            # reset counter after 5 failed attempts
+            menu()
+            error_count = 0
+        # otherwise run the program
         else:
-            print("Please select a valid option")
+            user_input = input("\nPlease select an option: ")
+            try:
+                user_input = int(user_input)
+            # check to make sure we are only accepting integer input for easy navigation. Pass to failure message
+            except ValueError:
+                pass
+
+            if user_input == 1:
+                error_count = 0
+                sqlite.create_table(cursor, table_name)  # debugging
+                print("table created")  # debugging
+            elif user_input == 2:
+                print("Let's add a user!\n")
+                error_count = 0
+                sqlite.add_user(cursor, table_name)  # debugging
+                menu()
+            elif user_input == 3:
+                print("option 3")
+                error_count = 0
+                sqlite.print_table(cursor, table_name)
+            elif user_input == 4:
+                print("option 4")
+                error_count = 0
+            elif user_input == 5:
+                print("\n----------------- FINAL TABLE ----------------")
+                sqlite.print_table(cursor, table_name)
+                # commit changes
+                conn.commit()
+                print("Changes committed")
+                # close the connection
+                conn.close()
+                print("Connection closed")
+                # end the program
+                break
+            # failure message
+            else:
+                error_count += 1
+                print("---------------- ERROR ---------------")
+                print(f"---- Error count: {error_count} -----")  # debugging
+                print("---- Please select a valid option ----\n")
+
+
+def menu():
+    print("--------------------- MENU --------------------")
+    print("1: Setup table and add base users  ------------")
+    print("2: Add new user  ------------------------------")
+    print("3: Print table  -------------------------------")
+    print("4: Report  ------------------------------------")
+    print("5: Commit changes and exit  -------------------")
 
 
 if __name__ == "__main__":
--- DataSpellProjects/school/ISIT333/sqlite3/sqlite.py
+++ DataSpellProjects/school/ISIT333/sqlite3/sqlite.py
@@ -1,48 +1,86 @@
 import sqlite3
 
-print("SQLite Practice - Example User Table\n")
-
 
 def connect(db_name):
     conn = sqlite3.connect(db_name)
-    print("Successfully connected\n")
-
     cursor = conn.cursor()
+    return conn, cursor
 
+
+def create_table(cursor, table_name):
     # check if the user table already exists, if so, drop it so we can start with a new table
-    cursor.execute("DROP TABLE IF EXISTS user;")
+    cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
 
     # create the table if it doesn't already exist
     # note that primary keys are automatically created in sqlit3 and referenced as rowid
-    cursor.execute("CREATE TABLE user (first_name TEXT, last_name TEXT, email TEXT, phone TEXT, address TEXT, "
+    cursor.execute(f"CREATE TABLE {table_name} (first_name TEXT, last_name TEXT, email TEXT, phone TEXT, address TEXT, "
                    "city TEXT, state TEXT, zipcode TEXT)")
 
-    # create some records of data
+    # create baseline records records of data
     cursor.execute(
-        "INSERT INTO user VALUES (\"Mia\", \"Wallace\", \"miwall@bandapart.com\",\"2068675309\",\"address\",\"city\",\"WA\",\"98052\")")
+        f"INSERT INTO {table_name} VALUES (\"Mia\", \"Wallace\", \"miwall@bandapart.com\",\"2068675309\",\"address\",\"city\",\"WA\",\"98052\")")
     cursor.execute(
-        "INSERT INTO user VALUES (\"Vincent\", \"Vega\", \"vvega@bandapart.com\",\"4258765309\",\"address\",\"city\",\"CA\",\"zipcode\")")
+        f"INSERT INTO {table_name} VALUES (\"Vincent\", \"Vega\", \"vvega@bandapart.com\",\"4258765309\",\"address\",\"city\",\"CA\",\"zipcode\")")
     cursor.execute(
-        "INSERT INTO user VALUES (\"Jules\", \"Winnfield\", \"jwinn@bandapart.com\",\"8048765309\",\"321 S Cherry St\",\"Richmond\",\"VA\",\"23220\")")
+        f"INSERT INTO {table_name} VALUES (\"Jules\", \"Winnfield\", \"jwinn@bandapart.com\",\"8048765309\",\"321 S Cherry St\",\"Richmond\",\"VA\",\"23220\")")
     cursor.execute(
-        "INSERT INTO user VALUES (\"Marsellus\", \"Wallace\", \"mawall@bandapart.com\",\"7578765309\",\"4918 SE Salmon St\",\"Portland\",\"OR\",\"zipcode\")")
+        f"INSERT INTO {table_name} VALUES (\"Marsellus\", \"Wallace\", \"mawall@bandapart.com\",\"7578765309\",\"4918 SE Salmon St\",\"Portland\",\"OR\",\"zipcode\")")
     cursor.execute(
-        "INSERT INTO user VALUES (\"Butch\", \"Coolidge\", \"bcool@bandapart.com\",\"3018765309\",\"123 Sad Lane\",\"Great Mills\",\"MD\",\"20634\")")
+        f"INSERT INTO {table_name} VALUES (\"Butch\", \"Coolidge\", \"bcool@bandapart.com\",\"3018765309\",\"123 Sad Lane\",\"Great Mills\",\"MD\",\"20634\")")
     cursor.execute(
-        "INSERT INTO user VALUES (\"Captain\", \"Koons\", \"ckoon@bandapart.com\",\"2028765309\",\"1600 Pennsylvania Avenue\",\"Distict of Columbia\",\"Distict of Columbia\",\"zipcode\")")
+        f"INSERT INTO {table_name} VALUES (\"Captain\", \"Koons\", \"ckoon@bandapart.com\",\"2028765309\",\"1600 Pennsylvania Avenue\",\"Distict of Columbia\",\"DC\",\"zipcode\")")
 
+    # WOULD LIKE TO ITERATE THROUGH DICTIONARY OF VALUES TO INPUT INTO SQL
+    # pulp_fiction: {
+    #     "Mia Wallace": {
+    #         "email": "",
+    #         "phone": "",
+    #         "address": "",
+    #         "city": "",
+    #         "state": "",
+    #         "zipcode": ""
+    #     }
+    # }
+
+
+def add_user(cursor, table_name):
+    user_firstname = input("First name: ")
+    user_lastname = input("Last name: ")
+    user_email = input("Email: ")
+    user_phone = input("Phone: ")
+    user_address = input("Address: ")
+    user_city = input("City: ")
+    user_state = input("State: ")
+    user_zipcode = input("Zipcode: ")
+
+    # pass in table name and establish query - would like to dynamically insert into all columns regardless of table
+    # like, "this table's columns"
+    sql = f"""
+        INSERT INTO {table_name} (first_name, last_name, email, phone, address, city, state, zipcode)
+        VALUES (?,?,?,?,?,?,?,?)
+    """
+
+    cursor.execute(sql, (user_firstname, user_lastname, user_email, user_phone,
+                         user_address, user_city, user_state, user_zipcode))
+
+    print(f"{user_firstname} has been added!")
+
+
+def print_table(cursor, table_name):
     # query the table including the rowid primary key value
-    cursor.execute("SELECT rowid, first_name, last_name, email FROM user")
+    cursor.execute(f"SELECT rowid, first_name, last_name, email, phone, address, city, state, zipcode FROM {table_name}")
 
     # store the results of a the query to a list called users
     users = cursor.fetchall()
-
+    print("{0:<3} {1: <15} {2: <20} {3: <25} {4: <12} {5: <30} {6: <20} {7: <7} {8: <5}".format(
+        "ID", "First name", "Last name", "Email", "Phone #", "Address", "City", "State", "Zipcode"
+        )
+    )
     # now we can loop through the results of the query
     for this_user in users:
-        print(this_user[0], this_user[1], this_user[2], this_user[3])
-
-    # save the updates to the database - if you don't commit any updates/inserts to the database will not be saved
-    conn.commit()
-
-    # close the connection
-    conn.close()
+        print("{0:<3} {1: <15} {2: <20} {3: <25} {4: <12} {5: <30} {6: <20} {7: <7} {8: <5}".format(
+            this_user[0], this_user[1], this_user[2], this_user[3],
+            this_user[4], this_user[5], this_user[6], this_user[7], this_user[8]
+            )
+        )
+    print()
