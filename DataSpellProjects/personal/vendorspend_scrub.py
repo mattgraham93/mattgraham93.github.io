6a9e126d65456a6a0461d24a73cde3cb448836ca
Matts Blade 15 <matt.graham@bellevuecollege.edu>
2021-12-18 17:43:48 -0800

    working to clean up portfolio

---
+++ DataSpellProjects/personal/vendorspend_scrub.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+
+# would like to get this into SQL, but let's start with just fixing the data
+from pandas import DataFrame
+
+file_name = "SAPVendorSpend.xlsx"
+file_save = "fixedSpend.xlsx"
+
+vsd = pd.read_excel(file_name)
+
+vsd.ReportingGC.unique()
+
+vsd = vsd.replace(["Skanska Balfour Beatty", "SBB"], "GC1", regex=True)
+vsd = vsd.replace("GLY Construction", "GC2", regex=True)
+vsd = vsd.replace("Sellen Constuction Company, Inc", "GC3", regex=True)
+
+subs = vsd["SubConsultant"]
+unique_subs = subs.unique()
+names = []
+
+for i in range(len(unique_subs)):
+    name = f"Sub{i+1}"
+    names.append(name)
+
+unique_sub_df: DataFrame = pd.DataFrame(unique_subs, columns=['GCName'])
+i = 0
+for sub in unique_subs:
+    name = names[i]
+    i += 1
+    vsd = vsd.replace(sub, name, regex=True)
+
+vsd
+
+print("---- writing to excel ----")
+try:
+    vsd.to_excel("test.xlsx")
+    print("---- file saved successfully ----")
+except Exception as e:
+    print(e)
