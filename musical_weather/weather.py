3ec3cd6b12806e53bad7b404455a352a651307d9
mattgraham93 <matt.graham@bellevuecollege.edu>
2024-05-05 13:26:09 -0700

    Refactor weather.py to include weighted weather score and analyze historical weather data

--- musical_weather/weather.py
+++ musical_weather/weather.py
@@ -10,20 +10,61 @@ import pandas as pd
 import senitment_analysis as sa
 import weather_historical as wh
 import weather_today as wt
+import numpy as np
 
 def analyze_weather(historical_weather):
     # Analyze the weather data
-    return historical_weather.groupby('description').agg(
-        {'temperature_2m_max': 'mean', 
-         'temperature_2m_min': 'mean', 
-         'temperature_2m_mean': 'mean',
-         'precipitation_sum': 'mean', 
-          'rain_sum': 'mean',
-         'wind_speed_10m_max': 'mean', 
-         'wind_gusts_10m_max': 'mean', 
-         'shortwave_radiation_sum': 'mean',
-         'weather_score': 'mean'}
-         ).reset_index()
+    condensed = pd.DataFrame(
+            historical_weather.groupby('description').agg(
+            {'temperature_2m_max': 'mean', 
+            'temperature_2m_min': 'mean', 
+            'temperature_2m_mean': 'mean',
+            'precipitation_sum': 'mean', 
+            'rain_sum': 'mean',
+            'daylight_duration': 'mean',  
+            'sunshine_duration': 'mean',
+            'precipitation_hours': 'mean',
+            'wind_speed_10m_max': 'mean', 
+            'wind_gusts_10m_max': 'mean', 
+            'shortwave_radiation_sum': 'mean',
+            'snowfall_sum': 'mean',
+            'weather_score': 'mean'}
+            ).reset_index()
+        )
+    
+    condensed['weather_score'] = condensed['weather_score'].fillna(1).astype(int)
+
+    condensed['base'] = condensed['daylight_duration'] + condensed['temperature_2m_mean']
+    condensed['good'] = condensed['sunshine_duration'] + condensed['shortwave_radiation_sum']
+    
+    condensed['bad'] = (condensed['daylight_duration'] - condensed['sunshine_duration']
+        ) + (
+            condensed['rain_sum'] * condensed['precipitation_hours']
+            ) + (
+                condensed['snowfall_sum'] * condensed['precipitation_hours']**2
+                )
+
+    condensed['weight'] = condensed['base'] + condensed['good'] - condensed['bad']
+
+    condensed['weight'] = np.where((condensed['precipitation_sum'] > 0) & (condensed['weight'] > 0), 
+                                condensed['weight'] * -1 ,
+                                condensed['weight']
+                                ) 
+
+    condensed['weather_score_weighted'] = np.where(condensed['weather_score'] < 0,
+                                    (abs(condensed['weather_score']) * condensed['weight']) + condensed['weight'],
+                                    (condensed['weather_score'] * condensed['weight']) + condensed['weight']
+                                   )
+
+    # condensed[['description', 
+    #            'weather_score', 
+    #            'good', 
+    #            'bad', 
+    #            'weight', 
+    #            'weather_score_weighted']
+    #            ].sort_values('weather_score_weighted', ascending=False)
+
+    return condensed.sort_values('weather_score_weighted', ascending=False)
 
 def store_weather_data(data):
     mongo_client = mongodb.get_client()
@@ -57,7 +98,7 @@ def get_weather_codes():
 
 def weather_main():
     # Set time period
-    start = '2000-01-01'
+    start = '2018-01-01'
     end = '2024-04-30'
     # # get and store latest data
     print('Getting weather data for Seattle')
@@ -71,12 +112,16 @@ def weather_main():
 
     historical_weather = pd.DataFrame(historical_weather)
     historical_weather['weather_code'] = historical_weather['weather_code'].astype(int)
+    print(f'Getting weather codes')
     weather_codes = get_weather_codes()
     weather_codes['weather_code'] = weather_codes['weather_code'].astype(int)
     weather_codes.drop(columns='image', inplace=True)
+    print(f'Analyzing weather data')
     joined = historical_weather.merge(weather_codes, on='weather_code', how='left')
     condensed = analyze_weather(joined)
-    print(condensed)
+    print(f"Top 5 weather conditions for Seattle:")
+    print(condensed.head(5))
+    return condensed
 
 if __name__ == '__main__':
     # weather_db = weather_main()
