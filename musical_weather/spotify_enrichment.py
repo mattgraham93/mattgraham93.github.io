a5e49e5650af53909ea8e391575a77528ba106f2
mattgraham93 <matt.graham@bellevuecollege.edu>
2024-05-08 23:11:42 -0700

    Refactor Spotify enrichment code in musical_weather module

--- musical_weather/spotify_enrichment.py
+++ musical_weather/spotify_enrichment.py
@@ -19,21 +19,56 @@ api: SpotifyApi = SpotifyApi(CLIENT_ID, CLIENT_SECRET, config)
 
 
 def get_song_uri(song, artist):
-    # search for the song
     print(f"Getting song info for {song}")
     songs = api.search.execute(f"{song},{artist}", ["track", "artist"])
+
+    if 'tracks' not in songs or not songs['tracks']['items']:
+        print(f"No tracks found for {song} by {artist}")
+        return None, None
+
     song_id = songs['tracks']['items'][0]['id']
     song_uri = songs['tracks']['items'][0]['uri']
     song_popularity = songs['tracks']['items'][0]['popularity']
-    return [song_uri, song_id, song_popularity]
-    
 
-def spotify_main():
-    song_location = get_song_uri('california', 'chappell roan')
-    song_id = song_location[1]
-    song_measures = api.tracks.audio_features(song_id)
-    
-    return song_measures, song_location
+    return song_uri, song_id, song_popularity    
 
-if __name__ == '__main__':
-    spotify_main()
+def get_playlist_tracks(playlist_id):
+    print(f"Getting tracks for playlist {playlist_id}")
+    tracks = api.playlists.get_playlist_items(playlist_id)
+
+    if 'items' not in tracks:
+        print(f"No tracks found for playlist {playlist_id}")
+        return None
+
+    track_info = []
+    for item in tracks['items']:
+        track = item['track']
+        track_uri = track['uri']
+        track_id = track['id']
+        track_popularity = track['popularity']
+        artist = track['artists'][0]['name']
+        track_title = track['name']
+        track_info.append((artist, track_title, track_uri, track_id, track_popularity))
+
+    return track_info
+
+# def spotify_main():
+#     playlist_id = '37i9dQZF1DX4aYNO8X5RpR'  # replace with your playlist id
+#     track_info = get_playlist_tracks(playlist_id)
+    
+#     for info in track_info:
+#         song_measures = api.tracks.audio_features(info[1])
+#         print(song_measures, info)
+
+# if __name__ == '__main__':
+#     spotify_main()
+
+# def spotify_main():
+#     song_location = get_song_uri('california', 'chappell roan')
+#     song_id = song_location[1]
+#     song_measures = api.tracks.audio_features(song_id)
+    
+#     return song_measures, song_location
+
+# if __name__ == '__main__':
+#     spotify_main()
