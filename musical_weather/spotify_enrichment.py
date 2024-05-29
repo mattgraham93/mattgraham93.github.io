c6540f94bb6b547350fcf36279f6b5b527ffb497
mattgraham93 <matt.graham@bellevuecollege.edu>
2024-05-28 19:20:36 -0700

    started flask transfirmation and added a new endpoint for the spotify enrichment

--- musical_weather/spotify_enrichment.py
+++ musical_weather/spotify_enrichment.py
@@ -2,9 +2,13 @@ import sys
 import json
 import spotify
 import pandas as pd
+import numpy as np
 import time
 
 from spotify_py_sdk import SpotifyApi, SdkConfig
+
+from sklearn.preprocessing import StandardScaler, PowerTransformer, QuantileTransformer
+from scipy.stats import skew
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -37,6 +41,103 @@ def get_song_uri(song, artist):
 def scale_score(weather_score, track_score, is_preciptitation):
     return weather_score * track_score
 
+
+def get_best_transformations(playlist_data, highly_skewed_columns, numerical_cols): 
+    best_transformations = {}
+
+    # Assuming highly_skewed_columns is your list of highly skewed columns
+    for column in highly_skewed_columns:
+        transformations = {
+            "original": playlist_data[column],
+            "st_scale": playlist_data[column + "_st_scale"],
+            "power_transform": playlist_data[column + "_power_transform"],
+            "quantile_transform": playlist_data[column + "_quantile_transform"]
+        }
+        # Calculate skewness for each transformation and find the one with smallest skewness
+        best_transform = min(transformations, key=lambda x: abs(skew(transformations[x])))
+        best_transformations[column] = best_transform
+
+    seen = set()
+    best_transformations_no_duplicates = {}
+
+    # Remove duplicate transformations
+    for column, transform in best_transformations.items():
+        if transform not in seen:
+            best_transformations_no_duplicates[column] = transform
+            seen.add(transform)
+    # Replace column names in base_columns with their best transformations if necessary
+    for i, column in enumerate(numerical_cols):
+        if column in best_transformations and best_transformations[column] != 'original':
+            numerical_cols[i] = column + '_' + best_transformations[column]
+    # Select the columns from standardized_data
+    selected_data = playlist_data[numerical_cols]
+    # Drop the base columns from playlist_data
+    playlist_data = playlist_data.drop(columns=numerical_cols, errors='ignore')
+    # Concatenate playlist_data with selected_data
+    playlist_data = pd.concat([playlist_data, selected_data], axis=1)
+
+    return playlist_data
+
+def get_all_transformations(playlist_df, numerical_cols):
+    # Initialize the transformers
+    scaler = StandardScaler()
+    power_transformer = PowerTransformer()
+    quantile_transformer = QuantileTransformer()
+
+    # Calculate skewness of each numerical column
+    skewness = playlist_df[numerical_cols].apply(lambda x: skew(x.dropna()))
+    # Convert skewness values to boolean values by comparing with threshold
+    skewness_bool = abs(skewness) > 0.5
+    # Ensure that skewness and skewness_bool have the same index
+    skewness_bool.index = skewness.index
+    # Filter out columns with skewness greater than a threshold (e.g., 0.5)
+    highly_skewed_columns = skewness[skewness_bool].index
+
+    # Apply StandardScaler, PowerTransformer, and QuantileTransformer to highly skewed columns
+    for column in highly_skewed_columns:
+        playlist_df.loc[:, column + "_st_scale"] = scaler.fit_transform(playlist_df[[column]])
+        playlist_df.loc[:, column + "_power_transform"] = power_transformer.fit_transform(playlist_df[[column]])
+        playlist_df.loc[:, column + "_quantile_transform"] = quantile_transformer.fit_transform(playlist_df[[column]])
+        
+    # Get the best transformations for the highly skewed columns
+    print("Getting best transformations for highly skewed columns")
+    playlist_df = get_best_transformations(playlist_df, highly_skewed_columns, numerical_cols)
+
+    return playlist_df
+
+def remove_outliers(playlist_data):
+    # Calculate Q1, Q3, and IQR
+    Q1 = playlist_data['duration_ms'].quantile(0.25)
+    Q3 = playlist_data['duration_ms'].quantile(0.75)
+    IQR = Q3 - Q1
+
+    # Define the outlier boundaries
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    # Create a new DataFrame that contains only the outliers
+    # outliers = playlist_data[(playlist_data['duration_ms'] < lower_bound) | (playlist_data['duration_ms'] > upper_bound)]
+
+    # Drop the outliers from the original DataFrame
+    playlist_data = playlist_data[(playlist_data['duration_ms'] >= lower_bound) & (playlist_data['duration_ms'] <= upper_bound)]
+
+    return playlist_data
+
+def transform_playlist_data(playlist_df, numerical_cols):
+    # Create a new column 'is_precipitation' that is 1 if the event is 'Rain', 'Snow', 'Storm', or 'Drizzle', and 0 otherwise
+    playlist_df['is_precipitation'] = playlist_df['event'].isin(['Rain', 'Snow', 'Storm', 'Drizzle']).fillna(False).astype(int)
+
+    # Remove outliers from the 'duration_ms' column
+    playlist_df = remove_outliers(playlist_df)
+
+    # Get the list of numerical columns
+    numerical_cols = playlist_df.select_dtypes(include=[np.number]).columns.tolist()
+
+    print("Applying transformations to highly skewed columns")
+    playlist_df = get_all_transformations(playlist_df, numerical_cols)
+
+    return playlist_df
+
 def calculate_base_score(playlist_data):
     playlist_data['base_score'] = playlist_data['duration_ms'] / playlist_data['tempo']
 
@@ -56,19 +157,6 @@ def calculate_base_score(playlist_data):
 
     return playlist_data
 
-def get_track_info(tracks):
-    track_info = []
-    for item in tracks['items']:
-        track = item['track']
-        if track is not None:
-            track_id = track.get('id')
-            track_popularity = track.get('popularity')
-            artist = track['artists'][0]['name'] if track.get('artists') else None
-            track_title = track.get('name')
-            if None not in [artist, track_title, track_id, track_popularity]:
-                track_info.append((artist, track_title, track_id, track_popularity))
-
-    return track_info
 
 def process_track_info(track_info, event, type_, api):
     # Initialize the data list and song counter
@@ -101,22 +189,21 @@ def process_track_info(track_info, event, type_, api):
                 song_counter += 1  # Increment song counter
                 print(f"\rProcessed Song {song_counter}: {info[0]} - Artist: {info[1]}", end="")
 
-    # Convert data_list to DataFrame
-    playlist_data = pd.DataFrame(data_list)
-    
-    # Create a new column 'is_precipitation' that is True if the event is 'Rain', 'Snow', 'Storm', or 'Drizzle'
-    playlist_data.loc[:, 'is_precipitation'] = playlist_data['event'].isin(['Rain', 'Snow', 'Storm', 'Drizzle'])
+    return data_list
 
-    # Fill NaN values with 0
-    playlist_data['is_precipitation'] = playlist_data['is_precipitation'].fillna(0)
+def get_track_info(tracks):
+    track_info = []
+    for item in tracks['items']:
+        track = item['track']
+        if track is not None:
+            track_id = track.get('id')
+            track_popularity = track.get('popularity')
+            artist = track['artists'][0]['name'] if track.get('artists') else None
+            track_title = track.get('name')
+            if None not in [artist, track_title, track_id, track_popularity]:
+                track_info.append((artist, track_title, track_id, track_popularity))
 
-    # Convert the boolean values to integers (True becomes 1, False becomes 0)
-    playlist_data.loc[:, 'is_precipitation'] = playlist_data['is_precipitation'].astype(int)
-
-    # Call calculate_base_score function
-    playlist_data = calculate_base_score(playlist_data)
-
-    return playlist_data
+    return track_info
 
 def get_playlist_tracks(playlist_id):
     print(f"Getting tracks for playlist {playlist_id}")
@@ -127,7 +214,6 @@ def get_playlist_tracks(playlist_id):
         return None
 
     return get_track_info(tracks)
-
 
 def process_playlists(playlists):
     # Initialize the dictionaries
@@ -161,10 +247,17 @@ def process_playlists(playlists):
         print(f"\nFinished processing playlist {playlist_id}")
         time.sleep(0.35)  # Add a delay of 0.33 seconds after each request
 
-    print("Creating DataFrame")
-    season_playlist_df = pd.DataFrame(data_list)  # Create DataFrame after the playlist loop
+    print("Finalizing playlist data")
+    playlist_df = pd.DataFrame(data_list)  # Create DataFrame after the playlist loop
 
-    return season_playlist_df, playlists_without_track_info, playlists_without_data
+    numerical_cols = playlist_df.select_dtypes(include=['int64', 'float64'])
+
+    print("Enhancing music data")
+    # Calculate base scores
+    playlist_df = transform_playlist_data(playlist_df, numerical_cols)
+    playlist_df = calculate_base_score(playlist_df)
+    
+    return playlist_df, playlists_without_track_info, playlists_without_data
 
 # def spotify_main():
 #     playlist_id = '37i9dQZF1DX4aYNO8X5RpR'  # replace with your playlist id
@@ -173,16 +266,6 @@ def process_playlists(playlists):
 #     for info in track_info:
 #         song_measures = api.tracks.audio_features(info[1])
 #         print(song_measures, info)
-
-# if __name__ == '__main__':
-#     spotify_main()
-
-# def spotify_main():
-#     song_location = get_song_uri('california', 'chappell roan')
-#     song_id = song_location[1]
-#     song_measures = api.tracks.audio_features(song_id)
-    
-#     return song_measures, song_location
 
 # if __name__ == '__main__':
 #     spotify_main()
